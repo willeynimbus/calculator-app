527a13e03125fa2cd09831080c999afb7697bc83
willeynimbus <ayushkmr14102003@gmail.com>
2025-07-16 10:36:55 +0530

    beautiful formated python code

--- test_calculator.py
+++ test_calculator.py
@@ -17,7 +17,7 @@ class TestCalculator(unittest.TestCase):
     def test_divide(self):
         self.assertEqual(divide(6, 2), 3)
         with self.assertRaises(ValueError):
-        divide(6, 0)
+            divide(6, 0)
 
 if __name__ == "__main__":
     unittest.main()
